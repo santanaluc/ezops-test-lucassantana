59bc5aebaf84820ebdf0a7cd5b440ad656065959
Lucas Santana <lucas.s.s.silva@hotmail.com>
2021-09-29 10:36:09 -0300

    initial commit

---
+++ server.js
@@ -0,0 +1,49 @@
+var express = require('express');
+var bodyParser = require('body-parser')
+var app = express();
+var http = require('http').Server(app);
+var io = require('socket.io')(http);
+var mongoose = require('mongoose');
+
+app.use(express.static(__dirname));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }))
+
+var Message = mongoose.model('Message', {
+    name: String,
+    message: String
+})
+
+var dbUrl = 'mongodb+srv://lslucas:mongodbtest@simple-chat.zxoln.mongodb.net/simple-chat?retryWrites=true&w=majority'
+
+app.get('/messages', (req, res) => {
+    Message.find({}, (err, messages) => {
+        res.send(messages);
+    })
+})
+
+app.get('/messages', (req, res) => {
+    Message.find({}, (err, messages) => {
+        res.send(messages);
+    })
+})
+
+app.post('/messages', (req, res) => {
+    var message = new Message(req.body);
+    message.save((err) => {
+        if (err)
+            sendStatus(500);
+        io.emit('message', req.body);
+        res.sendStatus(200);
+    })
+})
+
+io.on('connection', () => {
+    console.log('a user is connected')
+})
+
+mongoose.connect(dbUrl);
+
+var server = http.listen(3000, () => {
+    console.log('server is running on port', server.address().port);
+});
